b4b674b417fe4a1e17f126dc3bef7015677faada
Skablam <stainlesssteelmatt@gmail.com>
2015-03-23 10:16:24 +0000

    changed model to refer to records table rather than sor

--- manifests/rabbit.pp
+++ manifests/rabbit.pp
@@ -2,3 +2,14 @@ class { '::rabbitmq':
   service_manage    => true,
   port              => '5672',
 }
+
+rabbitmq_user { 'mqpublisher':
+ admin    => true,
+ password => 'mqpublisherpassword',
+}
+
+rabbitmq_user_permissions { 'mqpublisher@/':
+  configure_permission => '.*',
+  read_permission      => '.*',
+  write_permission     => '.*',
+}
