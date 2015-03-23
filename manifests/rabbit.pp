6b290086c6d65a2f4f43dd5669ffe652316519c3
treacle74 <ptrelease@gmail.com>
2015-03-23 11:18:49 +0000

    added rabbit user

--- manifests/rabbit.pp
+++ manifests/rabbit.pp
@@ -2,3 +2,9 @@ class { '::rabbitmq':
   service_manage    => true,
   port              => '5672',
 }
+
+rabbitmq_user { 'mqpublisher':
+ admin    => true,
+ password => 'mqpublisherpassword',
+ tags     => ['administrator'],
+}
