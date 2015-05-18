01fb5768c06cced8e3a1c3bc1858a60a55149b94
James Lademann <james.lademann@landregistry.gsi.gov.uk>
2015-05-18 13:21:53 +0100

    Reactivate environment at post-install start

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,7 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir
+source ~/venvs/system-of-record/bin/activate
 
 #Set environment variable in supervisord according to deploying environment (default to development)
 case "$DEPLOY_ENVIRONMENT" in
