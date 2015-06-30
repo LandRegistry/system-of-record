6ffc97529f41cd95f65fdf6177b1506b8f11371d
treacle74 <ptrelease@gmail.com>
2015-06-30 10:14:20 +0100

    removed space

--- environment.sh
+++ environment.sh
@@ -1,5 +1,5 @@
 export SQLALCHEMY_DATABASE_URI="postgresql://systemofrecord:systemofrecord@localhost/systemofrecord"
-export RABBIT_ENDPOINT ="amqp://mqpublisher:mqpublisherpassword@localhost:5672//"
+export RABBIT_ENDPOINT="amqp://mqpublisher:mqpublisherpassword@localhost:5672//"
 export RABBIT_QUEUE="system_of_record"
 export RABBIT_ROUTING_KEY="system_of_record"
 
