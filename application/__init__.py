d450e765e5d23dbb753223aa98faeff6d6ecb9f7
treacle74 <ptrelease@gmail.com>
2015-06-30 09:41:23 +0100

    only init queues for non tests.  only get develop config from env vars

--- application/__init__.py
+++ application/__init__.py
@@ -10,17 +10,18 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config.from_object(os.environ.get('SETTINGS'))
 
-#Configure the RabbitMQ connection, queue and producer.
-# By default messages sent to exchanges are persistent (delivery_mode=2),
-# and queues and exchanges are durable.
-# 'confirm_publish' means that the publish() call will wait for an acknowledgement.
-# # Producers are used to publish messages.
-exchange = Exchange()
-connection = Connection(hostname=app.config['RABBIT_ENDPOINT'], transport_options={'confirm_publish': True})
-queue = Queue(app.config['RABBIT_QUEUE'],
-              exchange,
-              routing_key=app.config['RABBIT_ROUTING_KEY'])(connection)
-queue.declare()
+if os.environ.get('SETTINGS') != "config.UnitTestConfig"
+    #Configure the RabbitMQ connection, queue and producer.
+    # By default messages sent to exchanges are persistent (delivery_mode=2),
+    # and queues and exchanges are durable.
+    # 'confirm_publish' means that the publish() call will wait for an acknowledgement.
+    # # Producers are used to publish messages.
+    exchange = Exchange()
+    connection = Connection(hostname=app.config['RABBIT_ENDPOINT'], transport_options={'confirm_publish': True})
+    queue = Queue(app.config['RABBIT_QUEUE'],
+                  exchange,
+                  routing_key=app.config['RABBIT_ROUTING_KEY'])(connection)
+    queue.declare()
 
-# Producers are used to publish messages.
-producer = Producer(connection)
+    # Producers are used to publish messages.
+    producer = Producer(connection)
