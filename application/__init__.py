18266fa7e9cb57cd07b7961c93b22cc88f231d14
treacle74 <ptrelease@gmail.com>
2015-06-30 12:33:41 +0100

    working on something else

--- application/__init__.py
+++ application/__init__.py
@@ -12,28 +12,18 @@ db = SQLAlchemy(app)
 app.config.from_object(os.environ.get('SETTINGS'))
 
 if os.environ.get('SETTINGS') != "config.UnitTestConfig":
-    #Configure the RabbitMQ connection, queue and producer.
+    #Configure the RabbitMQ connection, queue and producer for the flask app.
     # By default messages sent to exchanges are persistent (delivery_mode=2),
     # and queues and exchanges are durable.
-    # 'confirm_publish' means that the publish() call will wait for an acknowledgement.
-    # # Producers are used to publish messages.
     exchange = Exchange()
     connection = Connection(hostname=app.config['RABBIT_ENDPOINT'], transport_options={'confirm_publish': True})
-
     system_of_record_queue = Queue(app.config['RABBIT_QUEUE'],
                   exchange,
                   routing_key=app.config['RABBIT_ROUTING_KEY'])(connection)
     system_of_record_queue.declare()
-
-    republish_everything_queue = Queue(app.config['REPUBLISH_EVERYTHING_QUEUE'],
-                  exchange,
-                  routing_key=app.config['REPUBLISH_EVERYTHING_ROUTING_KEY'])(connection)
-    republish_everything_queue.declare()
-
     channel = connection.channel()
-
-    # Producers are used to publish messages.  Consumer consumes them.
     producer = Producer(connection)
 
+    # Check for and process republishing events.
     republish_all_titles(app, db)
 
