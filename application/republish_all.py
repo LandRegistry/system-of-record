193db1e81054def42a38d41e92496d7dd0608799
unknown <cs888pt@LRID500178.diti.lr.net>
2015-07-08 15:52:08 +0100

    amended to start republishing from a call to the endpoint - rather than as a polling service.  This avoids worker issues.

--- application/republish_all.py
+++ application/republish_all.py
@@ -19,11 +19,6 @@ def republish_all_titles(app, db):
     process_thread.setDaemon(True)
     process_thread.start()
 
-    # Thread to query the REPUBLISH_EVERYTHING_QUEUE and process any jobs found.
-    process_thread = threading.Thread(name='monitor-republish_queue', target=check_republish_everything_queue, args=(app, ))
-    process_thread.setDaemon(True)
-    process_thread.start()
-
 
 def check_for_republish_all_titles_file(app, db):
     republish_all_titles_file_exists = os.path.isfile(PATH)
@@ -31,22 +26,10 @@ def check_for_republish_all_titles_file(app, db):
         app.logger.audit('Republish everything: processing a request to republish all titles. ')
         process_republish_all_titles_file(app, db)
         remove_republish_all_titles_file(app)
-        check_for_republish_all_titles_file(app, db)
-    else:
-        time.sleep(5)
-        check_for_republish_all_titles_file(app, db)
 
 
 def process_republish_all_titles_file(app, db):
-    # Setup Rabbit queue connections
-    from kombu import Connection, Producer, Exchange, Queue
-    re_exchange = Exchange()
-    re_connection = Connection(hostname=app.config['REPUBLISH_EVERYTHING_ENDPOINT'], transport_options={'confirm_publish': True})
-    re_queue = Queue(app.config['REPUBLISH_EVERYTHING_QUEUE'],
-                                   re_exchange,
-                                   routing_key=app.config['REPUBLISH_EVERYTHING_ROUTING_KEY'])(re_connection)
-    re_queue.declare()
-    re_producer = Producer(re_connection)
+    from .server import republish_by_title_and_application_reference
 
     with open(PATH, "r") as read_progress_file:
         progess_data = json.load(read_progress_file)
@@ -55,13 +38,6 @@ def process_republish_all_titles_file(app, db):
     current_id = progess_data['current_id']
     last_id = progess_data['last_id']
 
-    def errback(exc, interval):
-        app.logger.error('Error publishing to queue: %r', exc, exc_info=1)
-        app.logger.info('Retry publishing in %s seconds.', interval)
-
-    # connection.ensure will re-establish the connection and retry, if the connection is lost.
-    publish_to_repubish_everything = re_connection.ensure(re_producer, re_producer.publish, errback=errback,
-                                                          max_retries=10)
     while current_id <= last_id:
         #get the title_number and application_reference for the id
         title_dict = get_title_detail(db, current_id)
@@ -69,11 +45,9 @@ def process_republish_all_titles_file(app, db):
             try:
                 title_number = title_dict['data']['title_number']
                 application_reference = title_dict['data']['application_reference']
-                queue_json = {"title_number": title_number, "application_reference": application_reference}
+                republish_json = {"title_number": title_number, "application_reference": application_reference}
 
-                publish_to_repubish_everything(queue_json, exchange=re_exchange,
-                                               routing_key=re_queue.routing_key, serializer='json',
-                                               headers={'title_number': title_number})
+                republish_by_title_and_application_reference(republish_json, False)
 
                 progress_count = progess_data['count']
                 progress_count += 1
@@ -94,25 +68,22 @@ def process_republish_all_titles_file(app, db):
         os.fsync(write_progress_file.fileno())  # Flush OS buffers
         write_progress_file.close()
 
-        # Upon success, rename to proper filename.  Rename is an atomic action.
-        max_tries = 10
+        # Upon success, rename to proper filename.  Rename is an atomic action.  May fail if the flask app is querying
+        # the progress file, to determine job progress.
+        max_tries = 100
+        loop_error = None
         for i in range(max_tries):
             try:
                 os.rename(TEMP_PATH, PATH)
                 break
             except Exception as err:
-                time.sleep(1)
-                log_republish_error(str(err), app)
+                time.sleep(0.1)
+                app.logger.info("cannot rename file.  On attempt %i" % i, app)
+                loop_error = err
         else:
-            log_republish_error('Can not rename temp file after processing id: %s.  Aborting job.' % current_id, app)
+            log_republish_error('Can not rename temp file after processing id: %s.  Aborting job.  Error: %s' % (current_id, str(loop_error)), app)
             #Abort this job by breaking out of the loop.  Otherwise it will continuously loop on the same id.
             break
-
-
-    last_job_notify_json = {"title_number": JOB_COMPLETE_FLAG, "application_reference": time.strftime("%b %d %Y %H:%M:%S") }
-    publish_to_repubish_everything(last_job_notify_json, exchange=re_exchange,
-                                   routing_key=re_queue.routing_key, serializer='json')
-    re_connection.close()
 
 
 def get_title_detail(db, the_id):
@@ -140,52 +111,6 @@ def remove_republish_all_titles_file(app):
             log_republish_error(str(err), app)
     else:
         log_republish_error('Can not remove job file after republishing', app)
-
-
-def process_message(body, message):
-    from application import app
-    from application.server import republish_by_title_and_application_reference
-
-    if body['title_number'] == JOB_COMPLETE_FLAG:
-        app.logger.audit('Republish everything: job completed at %s' % body['application_reference'])
-        message.ack()
-    else:
-        republish_by_title_and_application_reference(body, False)
-        message.ack()
-
-
-def running_as_service():
-    #Used by unit testing to stop looping
-    return True
-
-
-def check_republish_everything_queue(app):
-    # Setup Rabbit queue connections
-    from kombu import Connection, Exchange, Queue, Consumer
-    re_exchange = Exchange()
-    re_connection = Connection(hostname=app.config['REPUBLISH_EVERYTHING_ENDPOINT'], transport_options={'confirm_publish': True})
-    re_queue = Queue(app.config['REPUBLISH_EVERYTHING_QUEUE'],
-                     re_exchange,
-                     routing_key=app.config['REPUBLISH_EVERYTHING_ROUTING_KEY'])(re_connection)
-    re_queue.declare()
-    re_channel = re_connection.channel()
-
-    def errback(exc, interval):
-        app.logger.error('Error connecting to queue: %r', exc, exc_info=1)
-        app.logger.info('Retry connection in %s seconds.', interval)
-
-    re_consumer = Consumer(re_channel, queues=re_queue, callbacks=[process_message], accept=['json'])
-    re_consumer.consume()
-    # Loop "forever", as a service.
-    # N.B.: if there is a serious network failure or the like then this will keep logging errors!
-    while running_as_service():
-        try:
-            re_consumer.connection.ensure_connection(errback=errback, max_retries=10)
-            re_consumer.connection.drain_events()
-        except Exception as e:
-            log_republish_error("Exception trying to query REPUBLISH_EVERYTHING_QUEUE: %s" % e.args[0], app)
-            # If we ignore the problem, perhaps it will go away ...
-            time.sleep(10)
 
 
 def log_republish_error(message, app):
