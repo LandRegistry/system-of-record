081df9fd3906dee5c8acc460a3049a5176a0fa08
treacle74 <ptrelease@gmail.com>
2015-09-18 10:44:08 +0100

    readme and timestamo on table

--- application/models.py
+++ application/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer
+import datetime
+from sqlalchemy import Column, Integer, DateTime
 from sqlalchemy.dialects.postgresql import JSON
 from application import db
 
@@ -9,6 +10,7 @@ class SignedTitles(db.Model):
 
     id = Column(Integer, primary_key=True)
     record = db.Column(JSON)
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
 
     def __init__(self, record):
         self.record = record
