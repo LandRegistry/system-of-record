b8c0a1cfc8775239eee3c93de2a910c036168468
CS888PT <paul.trelease@landregistry.gsi.gov.uk>
2015-11-04 14:57:06 +0000

    only update progress every 10000 rows. More than doubles speed.  Unit tests still pass.

--- application/republish_all.py
+++ application/republish_all.py
@@ -13,6 +13,8 @@ TEMP_PATH = './republish_progress_tmp.json'
 JOB_COMPLETE_FLAG = 'all done'
 
 
+
+
 def republish_all_titles(app, db):
     # Thread to check the file for job progress and act accordingly.
     process_thread = threading.Thread(name='monitor-republish_file', target=check_for_republish_all_titles_file, args=(app, db, ))
@@ -59,31 +61,34 @@ def process_republish_all_titles_file(app, db):
                     current_id, str(err)), app)
 
         current_id += 1
-        progess_data['current_id'] = current_id
 
-        # Write progress to a temporary file 0
-        write_progress_file = open(TEMP_PATH, "w")
-        json.dump(progess_data, write_progress_file, ensure_ascii=False)
-        write_progress_file.flush()  # Flush Python buffers
-        os.fsync(write_progress_file.fileno())  # Flush OS buffers
-        write_progress_file.close()
+        # Update progress in the file every 10000 rows
+        if current_id % 10000 == 0 or current_id == last_id:
+            progess_data['current_id'] = current_id
+            write_progress_file = open(TEMP_PATH, "w")
+            json.dump(progess_data, write_progress_file, ensure_ascii=False)
+            write_progress_file.flush()  # Flush Python buffers
+            os.fsync(write_progress_file.fileno())  # Flush OS buffers
+            write_progress_file.close()
 
-        # Upon success, rename to proper filename.  Rename is an atomic action.  May fail if the flask app is querying
-        # the progress file, to determine job progress.
-        max_tries = 100
-        loop_error = None
-        for i in range(max_tries):
-            try:
-                os.rename(TEMP_PATH, PATH)
+            # Upon success, rename to proper filename.  Rename is an atomic action.  May fail if the flask app is querying
+            # the progress file, to determine job progress.
+            max_tries = 100
+            loop_error = None
+            for i in range(max_tries):
+                try:
+                    os.rename(TEMP_PATH, PATH)
+                    break
+                except Exception as err:
+                    time.sleep(0.1)
+                    app.logger.info("cannot rename file.  On attempt %i" % i, app)
+                    loop_error = err
+            else:
+                log_republish_error('Can not rename temp file after processing id: %s.  Aborting job.  Error: %s' % (current_id, str(loop_error)), app)
+                #Abort this job by breaking out of the loop.  Otherwise it will continuously loop on the same id.
                 break
-            except Exception as err:
-                time.sleep(0.1)
-                app.logger.info("cannot rename file.  On attempt %i" % i, app)
-                loop_error = err
-        else:
-            log_republish_error('Can not rename temp file after processing id: %s.  Aborting job.  Error: %s' % (current_id, str(loop_error)), app)
-            #Abort this job by breaking out of the loop.  Otherwise it will continuously loop on the same id.
-            break
+
+
 
 
 def get_title_detail(db, the_id):
