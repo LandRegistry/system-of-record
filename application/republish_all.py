712cdd27332c08fba7b4078d3d5f43d5c4b493db
CS888PT <paul.trelease@landregistry.gsi.gov.uk>
2015-11-06 15:05:41 +0000

    Further improvements to SQL call.

--- application/republish_all.py
+++ application/republish_all.py
@@ -12,6 +12,7 @@ TEMP_PATH = './republish_progress_tmp.json'
 
 JOB_COMPLETE_FLAG = 'all done'
 
+
 class RepublishTitles:
 
     def __init__(self):
@@ -41,71 +42,61 @@ class RepublishTitles:
 
 
     def process_republish_all_titles_file(self, app, db):
-        from .server import republish_by_title_and_application_reference
+        from .server import publish_json_to_queue
+        from application.models import SignedTitles
 
         with open(PATH, "r") as read_progress_file:
-            progess_data = json.load(read_progress_file)
+            progress_data = json.load(read_progress_file)
             read_progress_file.close()
 
-        current_id = progess_data['current_id']
-        last_id = progess_data['last_id']
+        current_id = progress_data['current_id']
+        progress_count = progress_data['count']
 
-        while current_id <= last_id:
-            #get the title_number and application_reference for the id
-            title_dict = self.get_title_detail(db, current_id)
-            if title_dict:
+        # 100 rows returned at a time. Start querying from offset value.
+        for row in db.session.query(SignedTitles).offset(current_id).yield_per(100):
+            if row:
                 try:
-                    title_number = title_dict['data']['title_number']
-                    application_reference = title_dict['data']['application_reference']
-                    republish_json = {"title_number": title_number, "application_reference": application_reference}
-
-                    republish_by_title_and_application_reference(republish_json, False)
-
-                    progress_count = progess_data['count']
+                    publish_json_to_queue(row.record, row.record['data']['title_number'])
                     progress_count += 1
-                    progess_data['count'] = progress_count
+                    progress_data['count'] = progress_count
+                    progress_data['current_id'] = row.id
 
                 except Exception as err:
                     self.log_republish_error(
-                        'Could not parse JSON to republish for row id %s owing to following error %s. ' % (
+                        'Could not republish for row id %s owing to following error %s. ' % (
                         current_id, str(err)), app)
+                    # Update the progress file upon error
+                    self.republish_all_progress(progress_data)
 
-            current_id += 1
+            # Update progress in the file for every 10000 processed rows
+            if progress_count % 10000 == 0:
+                self.republish_all_progress(progress_data)
 
-            # Update progress in the file every 10000 rows
-            if current_id % 10000 == 0 or current_id == last_id:
-                progess_data['current_id'] = current_id
-                write_progress_file = open(TEMP_PATH, "w")
-                json.dump(progess_data, write_progress_file, ensure_ascii=False)
-                write_progress_file.flush()  # Flush Python buffers
-                os.fsync(write_progress_file.fileno())  # Flush OS buffers
-                write_progress_file.close()
-
-                # Upon success, rename to proper filename.  Rename is an atomic action.  May fail if the flask app is querying
-                # the progress file, to determine job progress.
-                max_tries = 100
-                loop_error = None
-                for i in range(max_tries):
-                    try:
-                        os.rename(TEMP_PATH, PATH)
-                        break
-                    except Exception as err:
-                        time.sleep(0.1)
-                        app.logger.info("cannot rename file.  On attempt %i" % i, app)
-                        loop_error = err
-                else:
-                    self.log_republish_error('Can not rename temp file after processing id: %s.  Aborting job.  Error: %s' % (current_id, str(loop_error)), app)
-                    #Abort this job by breaking out of the loop.  Otherwise it will continuously loop on the same id.
-                    break
+        # Update the progress file upon completion
+        self.republish_all_progress(progress_data)
 
 
-    def get_title_detail(self, db, the_id):
-        from application.models import SignedTitles
-        a_dict = None
-        signed_titles_instance = db.session.query(SignedTitles).get(the_id)
-        if signed_titles_instance:
-            a_dict = signed_titles_instance.record
-        return a_dict
+    def republish_all_progress(self, progress_data):
+        write_progress_file = open(TEMP_PATH, "w")
+        json.dump(progress_data, write_progress_file, ensure_ascii=False)
+        write_progress_file.flush()  # Flush Python buffers
+        os.fsync(write_progress_file.fileno())  # Flush OS buffers
+        write_progress_file.close()
+
+        # Upon success, rename to proper filename.  Rename is an atomic action.  May fail if the flask app is querying
+        # the progress file, to determine job progress.
+        max_tries = 100
+        loop_error = None
+        for i in range(max_tries):
+            try:
+                os.rename(TEMP_PATH, PATH)
+                break
+            except Exception as err:
+                time.sleep(0.1)
+                app.logger.info("cannot rename file.  On attempt %i" % i, app)
+                loop_error = err
+        else:
+            self.log_republish_error('Can not rename temp file after processing id: %s.  Aborting job.  Error: %s' % (progress_data['current_id'], str(loop_error)), app)
 
 
     def remove_republish_all_titles_file(self, app):
